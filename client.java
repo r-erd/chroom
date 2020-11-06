fa1addcefb17e648887ca8e4a092e81a2f8ff042
r-erd <3ch5e77@gmail.com>
2020-11-06 07:09:45 +0100

    new comment

--- client.java
+++ client.java
@@ -4,23 +4,29 @@ import java.io.*;
 public class client{
     public static void main(String[] args) throws IOException {
 
-        Socket s = new Socket("localhost", 4999);
+        Socket socket = new Socket("localhost", 4999);
         System.out.print("\033[H\033[2J");
 
-        if (s.isConnected()){
+        if (socket.isConnected()){
             System.out.println("connected to server successfully");
         }
 
+        InputStream inputStream = socket.getInputStream();
+        OutputStream outputStream = socket.getOutputStream();
+
 
         //SENDING
-        PrintWriter pr = new PrintWriter(s.getOutputStream());
+        PrintWriter pr = new PrintWriter(outputStream);
         System.out.println("client : " + "its me - the client");
         pr.println("its me - the client");
         pr.flush();
 
 
+        //this is a new comment
+
+
         //RECEIVING
-        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        InputStreamReader in = new InputStreamReader(inputStream);
         BufferedReader bf = new BufferedReader(in);
         String str = bf.readLine();
         System.out.println("server : " + str);
